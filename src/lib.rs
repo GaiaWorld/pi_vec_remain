c4d76f1e92ad5efd586430f395382d9224db257c
zmax <zmythleo@gmail.com>
2024-12-13 18:03:16 +0800

    add remain_to

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,7 @@ use std::{
 
 pub trait VecRemain<R: RangeBounds<usize>> {
     fn remain(&mut self, range: R);
+    fn remain_to(&mut self, range: R, other: &mut Self);
 }
 
 impl<T, R: RangeBounds<usize>> VecRemain<R> for Vec<T> {
@@ -58,6 +59,55 @@ impl<T, R: RangeBounds<usize>> VecRemain<R> for Vec<T> {
             ptr.add(start).copy_to(ptr, self.len());
         }
     }
+    fn remain_to(&mut self, range: R, other: &mut Self) {
+        let end = match range.end_bound() {
+            Bound::Included(&end) => {
+                if end + 1 < self.len() {
+                    end + 1
+                } else {
+                    self.len()
+                }
+            }
+            Bound::Excluded(&end) => {
+                if end < self.len() {
+                    end
+                } else {
+                    self.len()
+                }
+            }
+            Bound::Unbounded => self.len(),
+        };
+        let start = match range.start_bound() {
+            Bound::Included(&start) => {
+                if start >= end {
+                    self.clear();
+                    return;
+                }
+                start
+            }
+            Bound::Excluded(&start) => {
+                if start >= end + 1 {
+                    self.clear();
+                    return;
+                }
+                start - 1
+            }
+            Bound::Unbounded => 0,
+        };
+        let count = end - start;
+        let ptr = self.as_mut_ptr();
+        unsafe {
+            // 释放start之前的内存
+            let s = ptr::slice_from_raw_parts_mut(ptr, start);
+            ptr::drop_in_place(s);
+
+            other.reserve(count);
+            ptr.add(start)
+                .copy_to_nonoverlapping(other.as_mut_ptr().add(other.len()), count);
+            other.set_len(other.len() + count);
+            self.set_len(0);
+        }
+    }
 }
 
 #[test]
@@ -74,4 +124,12 @@ fn test_vec_remain() {
     let mut vec = vec![1, 2, 3, 4, 5];
     vec.remain(..=3);
     assert_eq!(vec, &[1, 2, 3, 4]);
+    let mut vec = vec![1, 2, 3, 4, 5];
+    vec.remain_to(..=3, &mut vec![6, 7, 8, 9]);
+    assert_eq!(vec, &[]);
+    let mut vec = vec![1, 2, 3, 4, 5];
+    let mut other = vec![6, 7, 8, 9];
+    vec.remain_to(1..=3, &mut other);
+    assert_eq!(other, &[6, 7, 8, 9, 2, 3, 4]);
+    assert_eq!(vec, &[]);
 }
